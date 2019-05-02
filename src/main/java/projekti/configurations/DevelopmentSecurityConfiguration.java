d1486ea4380b694628c9f273024367ffbf73cf46
jhoneagle <eaglekjohn@gmail.com>
2019-05-02 13:41:41 +0300

    debugging

--- src/main/java/projekti/configurations/DevelopmentSecurityConfiguration.java
+++ src/main/java/projekti/configurations/DevelopmentSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.web.access.expression.DefaultWebSecurityExpr
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import projekti.utils.security.CustomPermissionEvaluator;
 
+@Profile({"development", "test"})
 @Configuration
 @EnableWebSecurity
 public class DevelopmentSecurityConfiguration extends WebSecurityConfigurerAdapter {

86b37c8cf5023d4123c89ba9034659ddcf147553
jhoneagle <eaglekjohn@gmail.com>
2019-05-02 20:20:14 +0300

    fix access

--- src/main/java/projekti/controllers/AccountRelatedAPI.java
+++ src/main/java/projekti/controllers/AccountRelatedAPI.java
@@ -17,41 +17,35 @@ public class AccountRelatedAPI {
     @Autowired
     private RestService restService;
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/old-face/api/ask")
     public FriendJson askToBeFriend(@RequestBody FriendJson friendJson) {
         this.restService.createFriendRequest(friendJson);
         return friendJson;
     }
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/old-face/api/ask/cancel")
     public FriendJson cancelRequest(@RequestBody FriendJson friendJson) {
         this.restService.cancelRequest(friendJson);
         return friendJson;
     }
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/old-face/api/request")
     public FriendJson handleRequest(@RequestBody FriendJson friendJson) {
         this.restService.handleRequest(friendJson);
         return friendJson;
     }
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/old-face/api/post")
     public List<CommentModel> createCommentForPost(@RequestBody ReactionJson reactionJson) {
         return this.restService.createCommentForPost(reactionJson);
     }
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/old-face/api/post/like")
     public ReactionJson likeOfThePost(@RequestBody ReactionJson reactionJson) {
         this.restService.addLikeToPost(reactionJson);
         return reactionJson;
     }
 
-    @CrossOrigin(origins = "/**")
     @PostMapping("/old-face/api/post/get")
     public List<CommentModel> getCommentsOfPost(@RequestBody ReactionJson reactionJson) {
         return this.restService.getCommentsOfPost(reactionJson);
